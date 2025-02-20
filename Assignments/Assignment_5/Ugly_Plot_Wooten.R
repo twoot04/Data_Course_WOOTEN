fee4fe8305fa1857d7ed70d0610f99b5ad8beebc
Taylor Wooten <wootentaylor04@gmail.com>
2025-02-19 23:51:33 -0700

    Ugly_Plot

---
+++ Assignments/Assignment_5/Ugly_Plot_Wooten.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+dat <- iris
+
+dat %>%
+  ggplot(aes(x = Petal.Width, 
+             y = Petal.Length)) + 
+  geom_point(color = "red", size = 12, shape = 5) + 
+  theme(panel.background = element_rect(fill = "#FFFF00"),
+        axis.title.x = element_text(size = 2, 
+                                    angle = 75,
+                                    vjust = .3, 
+                                    colour = "green"), 
+        axis.text.x = element_text(color = "lightblue", 
+                                   size = 4, 
+                                   angle = 83,), 
+        axis.title.y = element_text(size = 25, 
+                                    color = "turquoise", 
+                                    angle = -45), 
+        axis.text.y = element_text(color = "white", 
+                                   size = 4, 
+                                   angle = 2), 
+        plot.background = element_rect(fill = "green"), 
+        panel.grid.major = element_line(color = "blue", 
+                                        linetype = "dashed", 
+                                        size = 10), 
+        panel.grid.minor = element_line(size = 8, 
+                                        color = "orange", 
+                                        linetype = "twodash"))
+        
+
+
